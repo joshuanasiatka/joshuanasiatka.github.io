24c73d29c497da6b753e4465db3e14266d8c824b
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-11 23:29:11 -0500

    Upload Site

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Joshua Nasiatka</title>
+	<link href="tools/style.css" rel="stylesheet" type="text/css" />
+	<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<meta name="apple-mobile-web-app-title" content="Joshua Nasiatka">
+	<link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192">
+	<link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+	<meta name="msapplication-TileColor" content="#b91d47">
+	<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+	<meta name="application-name" content="Joshua Nasiatka">
+</head>
+<body>
+<div id="content_wrapper">
+	<div class="content" style="opacity:1">
+	<p class="center">
+	<a href="/files/resume/joshuanasiatka.pdf">
+	<img src="images/logo.png" width="40%" /></a>
+	<br /><br /><hr />
+	<img src="images/logo3.png" width="100%" /></p>
+		<hr />
+		<p class="resume"><br /><a href="/files/resume/joshuanasiatka.pdf">Snag my Resume</a> |
+			<a href="//bitcraftlabs.net">Bitcraft Labs</a></p>
+	</div>
+</div>
+</body>
+</html>
